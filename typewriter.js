6188bf2080dd0c346257332a4a04b806bf24f0c4
Michael Dever <mdever@live.ca>
2021-11-30 15:38:42 -0500

    Types out a given sentence one letter at a time

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+let sentence = "hello there from lighthouse labs";
+sentence += "\n";
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 50;
+}
